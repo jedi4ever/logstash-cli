0a5608c6cbdcddf624a0d5027013e8fecbe454b1
John A. Barbuto <jbarbuto@egnyte.com>
2012-06-12 07:51:57 +0000

    Made plain format the default

--- lib/logstash-cli/cli.rb
+++ lib/logstash-cli/cli.rb
@@ -17,7 +17,7 @@ module LogstashCli
     method_option :index_prefix , :default => "logstash-", :desc => "Logstash index prefix"
     method_option :from , :default => "#{Time.now.strftime('%Y-%m-%d')}", :desc => "Begin date"
     method_option :to, :default => "#{Time.now.strftime('%Y-%m-%d')}", :desc => "End date"
-    method_option :format , :default => 'csv', :desc => "Format to use for exporting (plain,csv,json)"
+    method_option :format , :default => 'plain', :desc => "Format to use for exporting (plain,csv,json)"
     method_option :size , :default => 500, :desc => "Number of results to return"
     method_option :last , :default => nil, :desc => "Specify period since now f.i. 1d"
     method_option :meta , :default => "type,message", :desc => "Meta Logstash fields to show"
@@ -32,7 +32,7 @@ module LogstashCli
     method_option :amqpurl, :default => 'amqp://logstash:foopass@localhost:5672', :desc => "URL to connect to AMQP"
     method_option :exchange, :default => 'rawlogs', :desc => "Exchange name"
     method_option :key, :default => '#', :desc => "Routing key"
-    method_option :format , :default => 'csv', :desc => "Format to use for exporting (plain,csv,json)"
+    method_option :format , :default => 'plain', :desc => "Format to use for exporting (plain,csv,json)"
     method_option :meta, :default => "timestamp,type,message", :desc => "Meta Logstash fields to show"
     method_option :delim, :default => "|", :desc => "csv delimiter"
 
