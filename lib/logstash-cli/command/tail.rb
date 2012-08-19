91359627c7d679045910bb32d8240929a8362275
John A. Barbuto <jbarbuto@egnyte.com>
2012-08-19 01:57:10 -0700

    Capture ctrl-c properly
    Ref: http://stackoverflow.com/questions/2089421/capturing-ctrl-c-in-ruby

--- lib/logstash-cli/command/tail.rb
+++ lib/logstash-cli/command/tail.rb
@@ -32,6 +32,8 @@ module Tail
       end
 
       AMQP.start(settings) do |connection, open_ok|
+        trap("INT") { puts "Shutting down..."; connection.close { EM.stop }; exit }
+
         channel = AMQP::Channel.new(connection, :auto_recovery => true)
 
         channel.queue("", :auto_delete => auto_delete, :peristent => persistent , :durable => durable)   do |queue, declare_ok|
@@ -52,10 +54,9 @@ module Tail
     rescue AMQP::PossibleAuthenticationFailureError => ex
       puts "Possible Authentication error:\nthe AMQP connection URL used is #{amqp_url}\n\nDetail Info:\n#{ex}"
       exit -1
-    rescue Exception => ex
-      puts "Error occured: #{ex}"
+    rescue StandardError => ex
+      puts "Error occurred: #{ex}"
       exit -1
     end
-    trap("INT") { puts "Shutting down..."; connection.close { EM.stop };exit }
   end
 end
